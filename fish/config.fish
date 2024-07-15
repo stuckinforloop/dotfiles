880ab43e173a3d2421d4b4211a556e7eb2d027e8
stuckinforloop <device.neel@gmail.com>
2024-07-15 14:54:53 +0530

    chore: add work changes

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,7 @@ source ~/.config/fish/alias.fish
 bind \cf tmux-sessionizer
 
 # env vars
-# source ~/.config/fish/env_vars.fish
+source ~/.config/fish/env_vars.fish
 
 zoxide init fish | source
 
@@ -19,8 +19,19 @@ set -gx HOMEBREW_NO_ENV_HINTS true
 set -gx GOPATH (go env GOPATH)
 set -gx PATH $PATH (go env GOPATH)/bin
 
+#php
+set -gx PATH $PATH /opt/homebrew/opt/php@8.1/bin
+set -gx PATH $PATH /opt/homebrew/opt/php@8.1/sbin
+
+#python
+set -gx PATH $PATH /Users/neel.modi/Library/Python/3.9/bin
+
 # 1pass
 set -gx SSH_AUTH_SOCK ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 
 # scripts
 set -gx PATH $PATH ~/.config/scripts
+
+set -gx PATH $PATH ~/.devstack/bin
+
+set -gx PATH $PATH ~/.volta/bin
