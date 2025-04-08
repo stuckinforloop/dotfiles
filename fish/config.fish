82b35ab1da1afc7049e0fdbe47aabb8a9c27221c
stuckinforloop <device.neel@gmail.com>
2025-04-08 14:18:10 +0530

    chore: april checkpoint

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,7 @@ source ~/.config/fish/alias.fish
 bind \cf tmux-sessionizer
 
 # env vars
-# source ~/.config/fish/env_vars.fish
+source ~/.config/fish/env_vars.fish
 
 zoxide init fish | source
 
@@ -16,6 +16,8 @@ set -gx HOMEBREW_NO_ENV_HINTS true
 # source "$HOME/.cargo/env.fish"
 
 # go
+source $HOME/.g/env
+set -gx GOBIN (go env GOPATH)/bin
 set -gx GOPATH (go env GOPATH)
 set -gx PATH $PATH (go env GOPATH)/bin
 
@@ -24,3 +26,25 @@ set -gx SSH_AUTH_SOCK ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/age
 
 # scripts
 set -gx PATH $PATH ~/.config/scripts
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/Users/stuckinforloop/.opam/opam-init/init.fish' && source '/Users/stuckinforloop/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# pnpm
+set -gx PNPM_HOME "/Users/stuckinforloop/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,43 +1,92 @@
 return {
-	"ellisonleao/gruvbox.nvim",
-	dependencies = {
-		"xiyaowong/transparent.nvim",
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = function()
+			require("gruvbox").setup({
+				bold = true,
+				dim_inactive = true,
+				invert_selection = true,
+				italic = {
+					strings = false,
+					emphasis = false,
+					comments = false,
+					operators = false,
+					folds = false,
+				},
+				transparent_mode = true,
+				terminal_colors = true,
+				underline = false,
+			})
+		end,
 	},
-	config = function()
-		require("gruvbox").setup({
-			bold = true,
-			dim_inactive = true,
-			invert_selection = true,
-			italic = {
-				strings = false,
-				emphasis = false,
-				comments = false,
-				operators = false,
-				folds = false,
-			},
-			transparent_mode = true,
-			terminal_colors = true,
-			underline = false,
-		})
-
-		function ColorMyPencils(color)
-			color = color or "gruvbox"
-			vim.cmd.colorscheme(color)
-			vim.opt.fillchars = { eob = " " }
-
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-			vim.api.nvim_set_hl(0, "Select", { bg = "none" })
-			vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none" })
-			vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
-
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		priority = 1000,
+		config = function()
+			require("rose-pine").setup({
+				disable_background = true,
+				styles = {
+					italic = false,
+				},
+				vim.cmd("colorscheme rose-pine-moon"),
+			})
+		end,
+	},
+	{
+		"xiyaowong/transparent.nvim",
+		priority = 1000,
+		config = function()
 			require("transparent").setup({
 				extra_groups = {
 					"NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
 				},
 			})
-		end
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+		opts = {
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		},
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			transparent_background = true,
+		},
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		priority = 1000,
+		opts = {
+			transparent = true,
+		},
+	},
+	-- {
+	-- 	event = "VeryLazy",
+	-- 	config = function()
+	-- 		function ColorMyPencils(color)
+	-- 			color = color or "rose-pine-moon"
+	-- 			vim.cmd.colorscheme(color)
+	-- 			vim.opt.fillchars = { eob = " " }
 
-		ColorMyPencils()
-	end,
+	-- 			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	-- 			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	-- 			vim.api.nvim_set_hl(0, "Select", { bg = "none" })
+	-- 			vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none" })
+	-- 			vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+	-- 		end
+
+	-- 		ColorMyPencils()
+	-- 	end,
+	-- },
 }
